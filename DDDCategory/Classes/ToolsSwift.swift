2f4f405043f74cfff1ffc24389667aa684c3a303
DaiSuke <feng.daisuke@gmail.com>
2020-10-22 11:29:46 +0800

    swift测试

---
+++ DDDCategory/Classes/ToolsSwift.swift
@@ -0,0 +1,15 @@
+//
+//  TempSwift.swift
+//  DSLinEngineeringManagement
+//
+//  Created by wells on 2020/10/20.
+//  Copyright © 2020 Haoran Liu. All rights reserved.
+//
+
+import UIKit
+
+class ToolsSwift: NSObject {
+    @objc func testPrint() {
+        print("fdsaf")
+    }
+}
